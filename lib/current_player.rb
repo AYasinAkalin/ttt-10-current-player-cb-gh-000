dd517023e47c183fcfe2809241423db063080dd0
A. Yasin A <ayasinakalin@gmail.com>
2018-06-09 11:38:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,9 @@
-def turn_count
-
+def turn_count(board)
+  turn = 0  # Number of turns played
+  board.each do |cell|
+    turn += 1 if ["X", "O"].include?(cell)
+  end
+  return turn
 end
 
 def current_player
