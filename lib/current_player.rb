efd46e5129445433539fbb7fdc76ab91a5864ea5
A. Yasin A <ayasinakalin@gmail.com>
2018-06-09 11:48:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,5 +7,12 @@ def turn_count(board)
 end
 
 def current_player(board)
+  moves = {player_X: 0, player_O: 0}
 
+  board.each do |cell|
+    moves[:player_X] += 1 if "X".include?(cell)
+    moves[:player_O] += 1 if "O".include?(cell)
+  end
+  
+  moves[:player_X] == moves[:player_O] ? "X" : "O"
 end
