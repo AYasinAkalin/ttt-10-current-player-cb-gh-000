b841e83eb6e7ded87cc74a449a2d576810acac51
A. Yasin A <ayasinakalin@gmail.com>
2018-06-09 11:49:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,7 +10,7 @@ def current_player(board)
   moves = {player_X: 0, player_O: 0}
 
   board.each do |cell|
-    moves[:player_X] += 1 if "X".include?(cell)
+    moves[:player_X] += 1 if "X".include?(cell) then next
     moves[:player_O] += 1 if "O".include?(cell)
   end
 
